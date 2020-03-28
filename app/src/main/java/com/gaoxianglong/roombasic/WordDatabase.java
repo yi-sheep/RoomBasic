531a330f9718983789cefdc32348516eec0edfb3
Your Name <you@example.com>
2020-03-28 16:13:19 +0800

    1

--- app/src/main/java/com/gaoxianglong/roombasic/WordDatabase.java
+++ app/src/main/java/com/gaoxianglong/roombasic/WordDatabase.java
@@ -6,5 +6,9 @@ import androidx.room.RoomDatabase;
 // entities这个指定实体，version这个指定版本，exportSchema指定导出模式
 @Database(entities = {Word.class},version = 1,exportSchema = false)
 public abstract class WordDatabase extends RoomDatabase {
+    /**
+     * 抽象方法，获取Dao对象
+     * @return
+     */
     public abstract WordDao getWordDao();
 }
